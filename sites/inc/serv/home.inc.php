1c3f3a6a600502b5ad51c9c83e83fc07f26a32fc
Kyri123 <olli143@live.de>
2020-04-14 17:32:24 +0200

    0.6.1 - Administratoren verwaltung
    - Administratoren verwaltung
    - Alten Code entfernt

--- sites/inc/serv/home.inc.php
+++ sites/inc/serv/home.inc.php
@@ -10,14 +10,40 @@ $user->setid($_SESSION['id']);
 $page_tpl->repl('cfg' ,$serv->show_name());
 $page_tpl->repl('SESSION_USERNAME' ,$user->name());
 
-/*
-$main_dir = $serv->get_save_dir(true);
-$file = $main_dir."/AllowedCheaterSteamIDs.txt";
-if(!file_exists($file)) file_put_contents($file, 76561198006463179);
-$array = file($file);
-echo file_get_contents($file);
-var_dump($array);
-*/
+$cheatfile = $serv->get_save_dir(true)."/AllowedCheaterSteamIDs.txt";
+
+//add_admin
+if(isset($_POST["addadmin"])) {
+    $id = $_POST["id"];
+    $api = $steamapi->getsteamprofile_class($id);
+    $content = file_get_contents($cheatfile)."\n$id";
+    if(file_put_contents($cheatfile, $content)) {
+        $resp = meld('success', "Admin <b>$api->personaname</b> wurde hinzugefügt!", 'Erfolgreich!', null);
+    }
+    else {
+        $resp = meld('danger', 'Fehler beim Schreiben der Datei!', 'Fehler!', null);
+    }
+}
+
+//remove Admin
+if(isset($url[4]) && isset($url[5]) && $url[4] == 'rm') {
+    $id = $url[5];
+    $api = $steamapi->getsteamprofile_class($id);
+    $content = file_get_contents($cheatfile);
+    if(substr_count($content, $id) > 0) {
+        $content = str_replace($id, null, $content);
+        if(file_put_contents($cheatfile, $content)) {
+            $resp = meld('success', "Admin <b>$api->personaname</b> wurde entfernt!", 'Erfolgreich!', null);
+        }
+        else {
+            $resp = meld('danger', 'Fehler beim Schreiben der Datei!', 'Fehler!', null);
+        }
+    }
+}
+
+
+
+
 $serv->cfg_read('arkserverroot');
 $savedir = $serv->get_save_dir();
 $player_json = $helper->file_to_json('data/saves/player_'.$serv->show_name().'.json', false);
@@ -25,56 +51,74 @@ $tribe_json = $helper->file_to_json('data/saves/tribes_'.$serv->show_name().'.js
 if(!is_array($player_json)) $player_json = array();
 if(!is_array($tribe_json)) $tribe_json = array();
 
-/*
-foreach ($array as $key => $value) {
-    $list_tpl = new Template('list_saves.htm', 'tpl/serv/sites/list/');
-    $list_tpl->load();
-    echo $key."<hr>";
-    if(in_array($value, $player_json)) {
-        for ($z = 0; $z < count($tribe_json); $z++) {
-            if($value == $player_json[$z]) $i = $z;
-            if($value == $player_json[$z]) break;
-        }
-    }
-    echo $i;
+//Liste für Admin
+if($serv->check_install() && !file_exists($cheatfile)) file_put_contents($cheatfile, "");
 
-    $pl = $jhelper->player($player_json, $i);
+$jhelper = new player_json_helper();
+$userlist_admin = null;
+$player_json = $helper->file_to_json('data/saves/player_'.$serv->show_name().'.json', false);
+if(!is_array($player_json)) $player_json = array();
 
-    if(is_array($tribe_json)) {
-        for ($z = 0; $z < count($tribe_json); $z++) {
-            $tribe = $jhelper->tribe($tribe_json, $z);
-            if ($tribe->Id == $pl->TribeId) {
-                $list_tpl->repl('tribe', $tribe->Name);
+$file = file($cheatfile);
+
+for ($i=0;$i<count($file);$i++) {
+    $find = array("\n", "\r", " ");
+    $file[$i] = str_replace($find, null, $file[$i]);
+    if(is_numeric($file[$i])) {
+        $list_tpl = new Template('list_user_admin.htm', 'tpl/serv/sites/list/');
+        $list_tpl->load();
+
+        $found = false;
+        for($p=0;$p<count($player_json);$p++) {
+            $pl = $jhelper->player($player_json, $p);
+            $id = $pl->SteamId;
+            if($id === $file[$i]) {
+                $found = true;
+                break;
             }
         }
+
+        if($found) {
+            $api = $steamapi->getsteamprofile_class($id);
+            $list_tpl->repl("igname", $pl->CharacterName);
+        }
+        else {
+            $api = $steamapi->getsteamprofile_class($file[$i]);
+            $list_tpl->repl("igname", "Ingamename Unbekannt (Savegame exsistiert nicht)");
+        }
+
+        $list_tpl->repl("stid", $api->steamid);
+        $list_tpl->repl("url", $api->profileurl);
+        $list_tpl->repl("cfg", $serv->show_name());
+        $list_tpl->repl("rndb", rndbit(25));
+        $list_tpl->repl("stname", $api->personaname);
+        $list_tpl->repl("img", $api->avatarmedium);
+
+        $adminlist_admin .= $list_tpl->loadin();
     }
-    $list_tpl->repl('tribe', '[Kein Stamm]');
-
-    if($pl->Level > 1000) $pl->Level = 0;
-    if($pl->TribeId == 7) $pl->TribeId = null;
-
-    $list_tpl->repl('IG:name', $pl->CharacterName);
-    $list_tpl->repl('IG:Level', $pl->Level);
-    $list_tpl->repl('lastupdate', converttime($pl->FileUpdated));
-    $list_tpl->repl('rnd', rndbit(10));
-    $list_tpl->repl('url', $steamapi->getsteamprofile_class($pl->SteamId)->profileurl);
-    $list_tpl->repl('img', $steamapi->getsteamprofile_class($pl->SteamId)->avatar);
-    $list_tpl->repl('steamname', $steamapi->getsteamprofile_class($pl->SteamId)->personaname);
-
-    $list_tpl->repl('rm_url', '/serverpage/'.$serv->show_name().'/saves/remove/'.$pl->SteamId.'.arkprofile');
-
-    $list_tpl->repl('EP', round($pl->ExperiencePoints, '2'));
-    $list_tpl->repl('SpielerID', $pl->Id);
-    $list_tpl->repl('TEP', $pl->TotalEngramPoints);
-    $list_tpl->repl('TID', $pl->TribeId);
-    $file = $savedir.'/'.$pl->SteamId.'.arkprofile';
-    $list_tpl->repl('durl', "/".$file);
-
-    $player .= $list_tpl->loadin();
-    $c_pl++;
 }
-*/
 
+if(is_array($player_json)) {
+    for($i=0;$i<count($player_json);$i++) {
+        $pl = $jhelper->player($player_json, $i);
+        $id = $pl->SteamId;
+        $name = $pl->SteamName;
+        $ig_name = $pl->CharacterName;
+        $not = true;
+        for($p=0;$p<count($file);$p++) {
+            if(intval($file[$p]) == $id) {
+                $not = false;
+                break;
+            }
+        }
+
+        if($not) $userlist_admin .= "<option value='$id'>$name - $ig_name</option>";
+    }
+}
+
+
+$page_tpl->repl("userlist_admin", $userlist_admin);
+$page_tpl->repl("adminlist_admin", $adminlist_admin);
 $page_tpl->rplSession();
 $panel = $page_tpl->loadin();
 
