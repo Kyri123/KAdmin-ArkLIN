5ade35c62ad15e12f527505c7069d2f7602cd6fc
Kyrium <olli143@live.de>
2020-06-17 17:23:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 */
 
 // hide errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 //error_reporting(E_ALL);
 
 //check install
