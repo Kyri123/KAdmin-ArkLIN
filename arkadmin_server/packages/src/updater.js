32e096f042450ab57757089ed6b7ecf949bb992b
Kyrium <olli143@live.de>
2020-08-05 15:54:45 +0200

    Updater fix

--- arkadmin_server/packages/src/updater.js
+++ arkadmin_server/packages/src/updater.js
@@ -21,13 +21,14 @@ exports.auto = (autoupdater) => {
                         console.log('\x1b[33m%s\x1b[0m', '[' + dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss") + '] Auto-Updater: \x1b[32mIst auf dem neusten Stand');
                     } else {
                         console.log('\x1b[33m%s\x1b[0m', '[' + dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss") + '] Auto-Updater: \x1b[36mUpdate wird gestartet');
-                        var command = 'screen -dm bash -c \'cd ' + config.WebPath + '/arkadmin_server/;' +
-                            'rm -R tmp; mkdir tmp; cd tmp;' +
-                            'wget https://github.com/Kyri123/Arkadmin/archive/' + config.autoupdater_branch + '.zip;' +
-                            'unzip ' + config.autoupdater_branch + '.zip; cd Arkadmin-' + config.autoupdater_branch + ';' +
-                            'rm -R ./arkadmin_server/config; ' +
-                            'rm -R ./install; ' +
-                            'rm ./install.php; ' +
+                        var command = 'screen -dm bash -c \'cd ' + config.WebPath + '/arkadmin_server/ ;' +
+                            'rm -R tmp ; mkdir tmp ; cd tmp ;' +
+                            'wget https://github.com/Kyri123/Arkadmin/archive/' + config.autoupdater_branch + '.zip ;' +
+                            'unzip ' + config.autoupdater_branch + '.zip; cd Arkadmin-' + config.autoupdater_branch + ' ;' +
+                            'rm -R ./arkadmin_server/config ; ' +
+                            'rm -R ./install ; ' +
+                            'rm ./install.php ; ' +
+                            'rm ./arkadmin_server/data/sha.txt ; ' +
                             'yes | cp -rf ./ ' + config.WebPath + '/ ;' +
                             'cd ../..; rm -R tmp; exit;\'';
                         shell.exec(command, config.use_ssh, 'Auto-Updater', true, 'Update wird gestartet');
