4d50dbe9cc8f406776119ffd1a18d8d1791ad05e
Kyrium <olli143@live.de>
2020-08-06 09:04:34 +0200

    Update config.inc.php

--- php/inc/config.inc.php
+++ php/inc/config.inc.php
@@ -21,6 +21,7 @@ $ip = $_SERVER['SERVER_ADDR'];
 $webserver['url'] = 'http://data.chiraya.de/';
 $webserver['changelog'] = $webserver['url'].'changelog.json';
 $webserver['config'] = json_decode(file_get_contents("arkadmin_server/config/server.json") ,true);
+$webserver['config']["port"] = (isset($webserver['config']["port"])) ? $webserver['config']["port"] : 30000;
 
 #Actions
 $action_opt = array(
