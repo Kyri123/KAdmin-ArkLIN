8ce697f99d710e7db1b5977a256b19d6b04eb159
Kyrium <olli143@live.de>
2020-08-20 15:12:29 +0200

    SQL anpassungen

--- app/sql/sql.sql
+++ app/sql/sql.sql
@@ -11,3 +11,7 @@ CREATE TABLE `ArkAdmin_user_cookies` (`id` int(10) unsigned zerofill NOT NULL AU
 DROP TABLE IF EXISTS `ArkAdmin_users`;
 CREATE TABLE `ArkAdmin_users` (`id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,`username` text,`email` text,`password` text,`lastlogin` double DEFAULT NULL,`registerdate` double DEFAULT NULL,`rang` int(11) DEFAULT NULL,`ban` int(11) DEFAULT '0',PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 INSERT INTO `ArkAdmin_users` VALUES ('0000000001', 'Gast', 'none', 'a', '0', '0', '0', '0');
+DROP TABLE IF EXISTS `ArkAdmin_players`;
+create table ArkAdmin_players (total_id int auto_increment, server text null, id bigint null, SteamId bigint null, SteamName text null, CharacterName text null, Level bigint null, ExperiencePoints bigint null, TotalEngramPoints bigint null, FirstSpawned boolean null, FileCreated bigint null, FileUpdated bigint null, TribeId text null, TribeName longtext null, constraint ArkAdmin_players_pk primary key (total_id));
+DROP TABLE IF EXISTS `ArkAdmin_tribe`;
+create table ArkAdmin_tribe (total_id int auto_increment, server text null, Id bigint null, tribeName text null, OwnerId bigint null, FileCreated bigint null, FileUpdated bigint null, Members longtext null, constraint ArkAdmin_tribe_pk primary key (total_id));
