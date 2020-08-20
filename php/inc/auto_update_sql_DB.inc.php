f7b78cdbde789785b02a266888dfd397301566ac
Kyrium <olli143@live.de>
2020-08-20 12:47:40 +0200

    Update auto_update_sql_DB.inc.php

--- php/inc/auto_update_sql_DB.inc.php
+++ php/inc/auto_update_sql_DB.inc.php
@@ -11,7 +11,7 @@
 //check SQL for Cookie_user
 $table = "ArkAdmin_user_cookies";
 $query_file = "app/sql/cookie_login.sql";
-if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
+if ($mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 0) {
     $query_file = file($query_file);
     foreach ($query_file as $query) {
         $mycon->query($query);
@@ -21,7 +21,7 @@ if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
 //check SQL for jobs
 $table = "ArkAdmin_jobs";
 $query_file = "app/sql/jobs.sql";
-if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
+if ($mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 0) {
     $query_file = file($query_file);
     foreach ($query_file as $query) {
         $mycon->query($query);
@@ -31,7 +31,7 @@ if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
 //check SQL for jobs
 $table = "ArkAdmin_shell";
 $query_file = "app/sql/shell.sql";
-if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
+if ($mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 0) {
     $query_file = file($query_file);
     foreach ($query_file as $query) {
         $mycon->query($query);
@@ -41,7 +41,7 @@ if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
 //check SQL for players
 $table = "ArkAdmin_players";
 $query_file = "app/sql/players.sql";
-if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
+if ($mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 0) {
     $query_file = file($query_file);
     foreach ($query_file as $query) {
         $mycon->query($query);
@@ -51,7 +51,7 @@ if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
 //check SQL for tribes
 $table = "ArkAdmin_tribe";
 $query_file = "app/sql/tribe.sql";
-if (!$mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 1) {
+if ($mycon->query("SHOW TABLES LIKE '$table'")->numRows() == 0) {
     $query_file = file($query_file);
     foreach ($query_file as $query) {
         $mycon->query($query);
