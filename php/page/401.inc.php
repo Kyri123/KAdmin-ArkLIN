fa15379f21482efcd67b498b811a180f00e1f281
Kyrium <olli143@live.de>
2020-09-04 07:43:16 +0200

    401

---
+++ php/page/401.inc.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * *******************************************************************************************
+ * @author:  Oliver Kaufmann (Kyri123)
+ * @copyright Copyright (c) 2019-2020, Oliver Kaufmann
+ * @license MIT License (LICENSE or https://github.com/Kyri123/Arkadmin/blob/master/LICENSE)
+ * Github: https://github.com/Kyri123/Arkadmin
+ * *******************************************************************************************
+*/
+
+
+// Vars
+$tpl_dir = 'app/template/core/error/';
+$setsidebar = false;
+
+$urls = NULL;
+$tpl = new Template("401.htm", $tpl_dir);
+$tpl->load();
+
+$content = $tpl->load_var();
+$pagename = "{::lang::php::err404::maintitle} 404";
+$pageicon = "<i class=\"fas fa-exclamation-triangle text-danger\" aria-hidden=\"true\"></i>";
+?>
