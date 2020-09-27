0849bfba61aa1f29446ccf674e01ccc74084cc50
Kyrium <olli143@live.de>
2020-09-27 12:11:30 +0200

    Update main.js

--- arkadmin_server/packages/src/webserver/router/main.js
+++ arkadmin_server/packages/src/webserver/router/main.js
@@ -29,9 +29,9 @@ function allowed(get, ip) {
         let user_path = `${config.WebPath}/app/json/user/${get.md5}.json`;
         if(fs.existsSync(user_path)) {
             let user_json = JSON.parse(fs.readFileSync(user_path));
-            if(fs.existsSync(user_json)) {
-                if(ip.includes(user_json.ip)) {
-                    let perm_path = `${config.WebPath}/app/json/user/${get.md5}.permissions.json`;
+            if(ip.includes(user_json.ip)) {
+                let perm_path = `${config.WebPath}/app/json/user/${get.md5}.permissions.json`;
+                if(fs.existsSync(perm_path)) {
                     let perm_json = JSON.parse(fs.readFileSync(perm_path));
                     console.log(perm_json)
                     return (perm_json.all.is_admin == 1 || (perm_json.all.manage_aas !== undefined ? perm_json.all.manage_aas == 1 : false));
