8d5141c52faa13347f9fc1cd8546bb260eae4bba
Kyrium <olli143@live.de>
2020-10-22 18:35:38 +0200

    Log Bugfix

--- app/dist/js/arkadmin2/sub_pages/servercenter/logs.js
+++ app/dist/js/arkadmin2/sub_pages/servercenter/logs.js
@@ -1,5 +1,5 @@
 setInterval(() => {
     getlog(`remote/serv/${vars.dirname}/arkmanager.log`, `${vars.cfg}`, `livelog_am`, `max`, `hide`, `filter`);
     getlog(`remote/serv/${vars.dirname}/arkserver.log`, `${vars.cfg}`, `livelog_ark`, `max`, `hide`, `filter`);
-    getlog(`remote/serv/${vars.dirname}/arkmanager.log`, `${vars.cfg}`, `livelog_mods`, `max`, `hide`, `filter`, `mods`);
+    getlog(`remote/serv/${vars.dirname}/arkmanager.log`, `${vars.cfg}`, `livelog_mods`, `max`, `hide`, `filter`, `mods_bool`);
 }, 1000);
