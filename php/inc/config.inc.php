c9bad9e3ba0a5ccb43400ecc41eb889496bea292
Kyrium <olli143@live.de>
2020-11-03 11:49:20 +0100

    Gameuserini > Duration

--- php/inc/config.inc.php
+++ php/inc/config.inc.php
@@ -13,7 +13,7 @@ if(file_exists(__ADIR__.'/php/inc/pconfig.inc.php')) include(__ADIR__.'/php/inc/
 
 // allgmeine
 $sitename = "ArkAdmin2";
-$version = '2.0.0';
+$version = '2.0.1';
 $ip = $_SERVER['SERVER_ADDR'];
 $maxpanel_server = 12;
 $buildid = 201.000;
